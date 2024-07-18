f34854c12753e028ebac3b8b8aa32c0632d16ee4
hippocampa <tsdhrm@outlook.com>
2024-07-18 11:55:09 +0800

    Features: Scrape from files
    What's new:
    
    1. Add `--files`
    2. Add `--verbose` and `--count`
    3. Implement `logging`

--- lugia.py
+++ lugia.py
@@ -10,10 +10,17 @@ from api.lugiascraper import LugiaScraper
 )
 @click.option("--start", default=2020, help="Scrape year start")
 @click.option("--end", default=2024, help="Scrape year end")
-def hello(id, file, start, end):
-    """Simple program that greets NAME for a total of COUNT times."""
-    scraper = LugiaScraper(id, file, start, end)
+@click.option("--verbose", is_flag=True, help="Will print verbose messages.")
+@click.option("--export-dir", help="Export directory")
+@click.option("--count", is_flag=True, help="Only count the number of articles")
+@click.option("--headless", is_flag=True, help="Run the scraper in headless mode", default=False)
+def hello(id, file, start, end, verbose, export_dir, count, headless):
+    """
+    Lugia: Scraping Author's Publication Data from Google Scholar
+    """
+    scraper = LugiaScraper(id, file, start, end, verbose, export_dir, count, headless)
     scraper.scrape()
+    scraper.quit()
 
 
 if __name__ == "__main__":
